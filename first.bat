32c52424603fcd249019d7aabaf7f24f99d4295f
myzty651 <dhanushkac085@gmail.com>
2025-05-01 22:49:58 +0530

    Add files via upload

---
+++ first.bat
@@ -0,0 +1,86 @@
+::2021ict82
+:: Learning Basic Commmanfd Promt & .Bat File  Operation That Enables us To work With CMD  
+
+:: Create A dictionary name Critieria_1 Using CMD Enter
+   mkdir Critieria_1
+
+:: Navigating To that Created Folder Using Cmd
+   cd Critieria_1
+
+:: Create subdirectories 'Standar_1' to 'Standar_5' inside 'Critieria_1'
+   mkdir Standar_1
+   mkdir Standar_2
+   mkdir Standar_3
+   mkdir Standar_4
+   mkdir Standar_5
+
+::Navigate back to the parent directory
+   cd ..
+
+::Create a directory named 'Critieria_2'
+   mkdir Critieria_2
+
+::Navigate into 'Critieria_2' directory
+   cd Critieria_2
+
+:: Create subdirectories 'Standar_1' to 'Standar_3' inside 'Critieria_2'
+   mkdir Standar_1
+   mkdir Standar_2
+   mkdir Standar_3
+
+::Move back to the parent directory
+   cd ..
+
+::Create a directory named 'Critieria_3'
+   mkdir Critieria_3
+
+::Navigate into 'Critieria_3' directory
+   cd Critieria_3
+
+::Create subdirectories 'Standar_1' to 'Standar_5' inside 'Critieria_3'
+   mkdir Standar_1
+   mkdir Standar_2
+   mkdir Standar_3
+   mkdir Standar_4
+   mkdir Standar_5
+
+::Move back to the parent directory
+   cd ..
+
+::Create a directory named 'Critieria_4'
+   mkdir Critieria_4
+
+::Create subdirectories 'Standar_1' to 'Standar_4' inside 'Critieria_4'
+   mkdir Standar_1
+   mkdir Standar_2
+   mkdir Standar_3
+   mkdir Standar_4
+
+::Move back to the parent directory
+   cd ..
+
+::Create a directory named 'Critieria_5'
+   mkdir Critieria_5
+
+::Navigate into 'Critieria_5' directory
+   cd Critieria_5
+
+::Create subdirectories 'Standar_1' and 'Standar_2' inside 'Critieria_5'
+   mkdir Standar_1
+   mkdir Standar_2
+
+::Move back to the parent directory
+   cd ..
+
+::Create a directory named 'Critieria_6'
+   mkdir Critieria_6
+
+::Navigate into 'Critieria_6' directory
+   cd Critieria_6
+
+::Create a subdirectory 'Standar_1' inside 'Critieria_6'
+   mkdir Standar_1
+
+
+
+
---
+++ second.bat
@@ -0,0 +1,21 @@
+
+:: Disable command showing in the result
+@echo off
+
+:: Displaying  the current username
+echo Username: %username%
+
+:: Displaying the version of Windows
+echo Windows Version: 
+
+:: Displaying the Windows version
+ver
+
+:: Display the current system date and prompt the user to change it
+date
+
+:: Display the current system time and prompt the user to change it
+time
+
+:: Pause the script execution until the user presses a key
+pause
